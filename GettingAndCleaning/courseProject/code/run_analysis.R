07cf990b6e810aa36d2b34541ee1797299cf4d85
Rosario Martinez <rosario.mgomez@gmail.com>
2014-09-10 15:42:20 -0700

    implemented part 3 of the analysis

--- GettingAndCleaning/courseProject/code/run_analysis.R
+++ GettingAndCleaning/courseProject/code/run_analysis.R
@@ -1,11 +1,11 @@
 #--------------------------
-# 0. Download the data
+# 0. Download the data and stores it in the data/raw folder
 #--------------------------
 source("code/downloadData.R")
 downloadData()
 
 #--------------------------
-# 1. Merges the training and the test sets to create one data set
+# 1. Merge the training and the test sets to create one data set
 #--------------------------
 X_train <- read.table("data/raw/UCI HAR Dataset/train/X_train.txt", sep = '',
                     header = FALSE)
@@ -16,20 +16,23 @@ X_test <- read.table("data/raw/UCI HAR Dataset/test/X_test.txt", sep = '',
 X <- rbind(X_train, X_test)
 
 #--------------------------
-# 2. Extracts only the measurements on the mean and standard deviation for each 
-#    measurement
-#--------------------------
-
-#####TODO
-
-#--------------------------
-#3. Appropriately labels the data set with descriptive variable names
+#2. Appropriately label the data set with descriptive variable names
 #--------------------------
 featureNames <- read.table("data/raw/UCI HAR Dataset/features.txt", sep = '',
                            col.names = c('num', 'name'), header = FALSE,
                            stringsAsFactors = FALSE)
 
 colnames(X) <- featureNames$name
+
+#--------------------------
+# 3. Extracts only the measurements on the mean and standard deviation for each 
+#    measurement
+#--------------------------
+
+col_means <- grep("-mean()", colnames(X))
+col_stds  <- grep("-std()", colnames(X))
+cols <- sort(c(col_means, col_stds))
+X_m_sd <- X[,cols]
 
 #--------------------------
 #4. Uses descriptive activity names to name the activities in the data set
@@ -48,7 +51,12 @@ ylabels <- read.table("data/raw/UCI HAR Dataset/activity_labels.txt", sep = '',
 
 ###PENDING TO ASIGN ylabels to y
 
+# save the tidy dataset into a file
+
+
 #--------------------------
 #5. Create a second independent tidy data set with the average of each variable
 #   for each activity and each subject
 #--------------------------
+
+#save the tidy dataset into a file

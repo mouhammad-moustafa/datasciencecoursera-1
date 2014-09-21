ae4e155edfe5f007778e5b707695a6362f89f10a
Rosario Martinez <rosario.mgomez@gmail.com>
2014-09-21 15:35:52 -0700

    minor changes

--- DevDataProducts/ui.R
+++ DevDataProducts/ui.R
@@ -5,30 +5,32 @@ library(rCharts)
 data(BostonHousing2)
 towns <- sort(as.character(unique(BostonHousing2$town)))
 
-shinyUI(navbarPage("BostonState",
+shinyUI(navbarPage("BostonRE",
     tabPanel("Predict",
              fluidPage(
-                 h4("Welcome to the Boston home prices page!"),
-                 p("With this application, you can check what is the predicted price
-                    of the houses in Boston suburbs based on your data input"),
                  fluidRow(
                     column(3,
+                        h4("Welcome to the BostonRE!"),
                         wellPanel(
                              selectInput('town', "Town name:", choices=towns),
-                             hr(),
                              numericInput('rooms', 'Number of rooms', 1, min = 1, max = 50, step = 1),
                              numericInput('age', 'Years of the house', 0, min = 0, max = 150, step = 1),
                              actionButton('submitBut', 'Submit')  
                          ),
                         hr(),
                         h4("Houses in town:"),
-                        verbatimTextOutput("numHouses")
+                        verbatimTextOutput("numHouses"),
+                        hr(),
+                        p("This application builds a prediction model to estimate the house prices in the Boston area. You 
+                           can obtain a price estimate by selecting the different features on the panel above and click Submit."),
+                        p("The model is based on the BostonHousing2 data from the mlbench library.")
                       ),
                       column(4, offset = 1,
                            h4('Estimated price for your preferences:'),
                            verbatimTextOutput('prediction'),
-                           h4('Price summary in town:'),
+                           h4('Price summary in your town search:'),
                            verbatimTextOutput('priceSum'),
+                           h4('Houses location and prices in your town search:')
                            tags$style('.leaflet {width: 200px;}'),
                            showOutput('houseMap', 'leaflet')
                       )

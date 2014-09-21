e0515331d6b09206e646b6d511e1c9aea0564fe9
Rosario Martinez <rosario.mgomez@gmail.com>
2014-09-21 09:14:51 -0700

    shiny app code

--- DevDataProducts/server.R
+++ DevDataProducts/server.R
@@ -1,5 +1,58 @@
 library(shiny)
+library(ggplot2)
+library(mlbench)
+library(rCharts)
+library(dplyr)
+
+data(BostonHousing2)
+modFit <- lm(medv ~ age + rm, data = BostonHousing2)
+
+housePrice <- function(hage, rooms) {
+    dt <- data.frame(age = hage, rm = rooms)
+    predict(modFit, newdata = dt )
+}
+
 shinyServer(
     function(input, output) {
+        output$prediction <- renderText({ 
+            if (input$submitBut == 0) { "Please, select a town" }
+            else {
+                input$submitBut
+                isolate(housePrice(input$age, input$rooms))
+            }
+        })
+        #reactive code returns a function not a df
+        town_filter <- reactive({
+            filter(BostonHousing2, town == input$town)
+        })
+        output$numHouses <- renderText({
+            if (input$submitBut == 0) { }
+            else{
+                input$submitBut
+                isolate(nrow(town_filter()))
+            }
+        })
+        output$priceSum <- renderPrint({
+            if (input$submitBut == 0) { }
+            else{
+                input$submitBut
+                isolate(summary(town_filter()$medv))
+            }
+        })
+        output$houseMap <- renderMap({
+            map3 <- Leaflet$new()
+            map3$setView(c(42.3, -71), zoom = 9)
+            if (input$submitBut != 0) {
+                if (nrow(town_filter()) > 0) {
+                    map3$setView(c(town_filter()$lat[1], town_filter()$lon[1]), zoom = 12)
+                    for (i in 1:nrow(town_filter())){
+                        map3$marker(c(town_filter()$lat[i], town_filter()$lon[i]),
+                                    bindPopup = paste('Price: <br>', 
+                                                      town_filter()$medv[i]*1000, '$',sep=""))
+                    }
+                }
+            }
+            map3
+        })
     }
 )
--- DevDataProducts/ui.R
+++ DevDataProducts/ui.R
@@ -1,10 +1,41 @@
-shinyUI(pageWithSidebar(
-    headerPanel("Foo"),
-    sidebarPanel(
-        h1("Llala")
-    ),
-    mainPanel(
-        h3("Main panel"),
-        code("some code here")
-    )
+library(shiny)
+library(ggplot2)
+library(mlbench)
+library(rCharts)
+data(BostonHousing2)
+towns <- sort(as.character(unique(BostonHousing2$town)))
+
+shinyUI(navbarPage("BostonState",
+    tabPanel("Predict",
+             fluidPage(
+                 h4("Welcome to the Boston home prices page!"),
+                 p("With this application, you can check what is the predicted price
+                    of the houses in Boston suburbs based on your data input"),
+                 fluidRow(
+                    column(3,
+                        wellPanel(
+                             selectInput('town', "Town name:", choices=towns),
+                             hr(),
+                             numericInput('rooms', 'Number of rooms', 1, min = 1, max = 50, step = 1),
+                             numericInput('age', 'Years of the house', 0, min = 0, max = 150, step = 1),
+                             actionButton('submitBut', 'Submit')  
+                         ),
+                        hr(),
+                        h4("Houses in town:"),
+                        verbatimTextOutput("numHouses")
+                      ),
+                      column(4, offset = 1,
+                           h4('Estimated price for your preferences:'),
+                           verbatimTextOutput('prediction'),
+                           h4('Price summary in town:'),
+                           verbatimTextOutput('priceSum'),
+                           tags$style('.leaflet {width: 200px;}'),
+                           showOutput('houseMap', 'leaflet')
+                      )
+                    )
+                 )
+            ),
+           tabPanel('DataSet Info',
+                    p('summary')
+           )
 ))
